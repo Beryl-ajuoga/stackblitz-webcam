bf7b18eb7452ff63f4a3be9037abd79e22d7f698
Beryl-ajuoga <berrylajuoga@gmail.com>
2023-08-25 16:48:52 +0300

    stackblitz update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CameraComponent } from './camera/camera.component';
+import { FormsModule } from '@angular/forms';
+import { WebcamModule } from 'ngx-webcam';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CameraComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/camera/camera.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
+
+@Component({
+  selector: 'app-camera',
+  templateUrl: './camera.component.html',
+  styleUrls: ['./camera.component.css'],
+})
+export class CameraComponent implements OnInit {
+  @Output()
+  public pictureTaken = new EventEmitter<WebcamImage>();
+
+  // camera turning on and off
+  public showWebcam = true;
+  public allowCameraSwitch = true;
+  public multipleWebcams = false;
+  public deviceId!: string;
+  public videoOptions: MediaTrackConstraints = {};
+  public errors: WebcamInitError[] = [];
+
+  // webcam camera trigger
+  private trigger: Subject<void> = new Subject<void>();
+
+  // switching camera back and forth
+  private nextWebcam: Subject<boolean | string> = new Subject<
+    boolean | string
+  >();
+
+  public ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    WebcamUtil.getAvailableVideoInputs().then(
+      (MediaDevices: MediaDeviceInfo[]) => {
+        this.multipleWebcams = MediaDevices && MediaDevices.length > 1;
+      }
+    );
+  }
+
+  public triggerCamera(): void {
+    this.trigger.next();
+  }
+
+  public toggleWebcam(): void {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  public handleInitError(error: WebcamInitError): void {
+    this.errors.push(error);
+  }
+
+  public showNextWebcam(directionOrDeviceId: boolean | string): void {
+    this.nextWebcam.next(directionOrDeviceId);
+  }
+
+  public handleImage(webcamImage: WebcamImage): void {
+    this.pictureTaken.emit(webcamImage);
+  }
+
+  public cameraWasSwitched(deviceId: string): void {}
+
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean | string> {
+    return this.nextWebcam.asObservable();
+  }
+}
